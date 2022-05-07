1aa1b20fcba09387bef400ea53f2db3c2c9191c1
Ian Cowan <icowan@ucsc.edu>
2022-05-07 11:55:39 -0700

    added walls

--- src/scene/NodeTwo.js
+++ src/scene/NodeTwo.js
@@ -14,33 +14,39 @@ class NodeTwo extends Phaser.Scene {
         controls.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT); 
         this.nate = this.physics.add.sprite(game.config.width / 2, game.config.height / 2, "astro");
         this.nate.setInteractive({draggable: true});
+        this.nate.setCollideWorldBounds(true);
+        this.nate.setBounceX(0.8);
+        this.nate.setBounceY(0.8);
+    
         let firstFlingDrag = true;
         let flingStart = {x: 0, y: 0};
         let flingEnd = {x: 0, y: 0};
-        
         this.nate.on('drag', (pointer, dragX, dragY, dropped) => {
             if (firstFlingDrag) {
                 firstFlingDrag = false;
                 flingStart.x = dragX;
                 flingStart.y = dragY;
-
                 this.flingLine = this.add.line(0, 0, flingStart.x, flingStart.y, flingStart.x, flingStart.y, 0x00ff00)
             } else {
-                flingEnd.x = dragX
-                flingEnd.y = dragY
-                
+                flingEnd.x = dragX;
+                flingEnd.y = dragY;
                 this.flingLine.setTo(flingStart.x, flingStart.y, flingEnd.x, flingEnd.y);
             }
         })
         this.nate.on('dragend', (pointer, dragX, dragY, dropped) => {
             console.log("end", dragX, dragY);
-            let vectorX = flingEnd.x - flingStart.x
-            let vectorY = flingEnd.y - flingStart.y
+            let vectorX = flingEnd.x - flingStart.x;
+            let vectorY = flingEnd.y - flingStart.y;
             this.nate.setVelocityX(vectorX);
             this.nate.setVelocityY(vectorY);
             this.flingLine.destroy();
             firstFlingDrag = true;
         })
+
+        this.walls = this.physics.add.staticGroup();
+        this.walls.add(this.add.rectangle(0, game.config.height / 4, game.config.width, 20, 0xffffff).setOrigin(0, 0));
+        this.walls.add(this.add.rectangle(0, game.config.height / 4 + 100, game.config.width, 20, 0xffffff).setOrigin(0, 0));
+        this.physics.add.collider(this.nate, this.walls, null, null, this);
 
     }
 
