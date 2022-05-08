1091f367391bbf9c30e7a3f4d94536c497243355
Ian Cowan <icowan@ucsc.edu>
2022-05-08 00:39:20 -0700

    camera-window following

--- src/scene/NodeTwo.js
+++ src/scene/NodeTwo.js
@@ -14,10 +14,11 @@ class NodeTwo extends Phaser.Scene {
         controls.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT); 
         this.nate = this.physics.add.sprite(game.config.width / 2, game.config.height / 2, "astro");
         this.nate.setInteractive({draggable: true});
-        this.nate.setCollideWorldBounds(true);
+        //this.nate.setCollideWorldBounds(true);
         this.nate.setBounceX(0.8);
         this.nate.setBounceY(0.8);
     
+        // flinging controls
         let firstFlingDrag = true;
         let flingStart = {x: 0, y: 0};
         let flingEnd = {x: 0, y: 0};
@@ -43,11 +44,14 @@ class NodeTwo extends Phaser.Scene {
             firstFlingDrag = true;
         })
 
+        // walls
         this.walls = this.physics.add.staticGroup();
         this.walls.add(this.add.rectangle(0, game.config.height / 4, game.config.width, 20, 0xffffff).setOrigin(0, 0));
         this.walls.add(this.add.rectangle(0, game.config.height / 4 + 100, game.config.width, 20, 0xffffff).setOrigin(0, 0));
         this.physics.add.collider(this.nate, this.walls, null, null, this);
 
+        this.cameras.main.startFollow(this.nate, true, 0.1, 0.1);
+        this.cameras.main.setDeadzone(150, 150);
     }
 
     update() {
