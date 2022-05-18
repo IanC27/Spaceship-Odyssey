7440aecfe2390eb40b7eede8cbe08e1bf9c56e00
Ian Cowan <icowan@ucsc.edu>
2022-05-17 23:57:49 -0700

    trim unnecessary code & features

--- src/prefab/SleepingBag.js
+++ src/prefab/SleepingBag.js
@@ -18,20 +18,15 @@ class SleepingBag extends Activity {
             game.clock.minutes -= 60;
         }
 
-        this.hoursSlept = Math.floor((this.sleepStartTime - game.clock.minutes) / 60)
-        this.sleepIndicator.text = `asleep for: ${this.hoursSlept} hours`;        
-
     }
 
     onInteract(player){
         this.text = []
         this.text.push(this.scene.add.text(this.x, this.y - 50, 'press q to wake up', {fontSize: '10px',}).setOrigin(0.5, 0.5)); 
-        this.text.push(this.scene.add.text(this.x, this.y - 40, 'press e to fast forward', {fontSize: '10px',}).setOrigin(0.5, 0.5));
         
         this.scene.awakeTimer.paused = true;
         this.scene.sleepTimer.paused = false;
 
-        this.sleepIndicator = this.scene.add.text(this.x, this.y - 30, 'asleep for: 0 hours', {fontSize: '10px',}).setOrigin(0.5, 0.5);
         this.sleepStartTime = game.clock.minutes;
         this.hoursSlept = 0;
         
@@ -43,10 +38,7 @@ class SleepingBag extends Activity {
             tex.destroy()
         }
         this.setFrame(0)
-        if (playerStatus.wellRested) {
-            console.log("rested well!");
-        }
-        this.sleepIndicator.destroy();
+        
         this.scene.awakeTimer.paused = false;
         this.scene.sleepTimer.paused = true;
     }
